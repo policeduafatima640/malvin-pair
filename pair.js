aaab10b7b11eff78eed239a04d64393a4c61d41f
policeduafatima640 <policeduafatima64@gmail.com>
2025-07-15 14:09:06 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -77,9 +77,9 @@ var randomItem = selectRandomItem(items);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
                         let md = "fatima~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
-                        let desc = `*Hey there, FATIMA-MD User!* 👋🏻
+                        let desc = `*Hey there, MALVIN-XD User!* 👋🏻
 
-Thanks for using *DUA-FATIMA* — your session has been successfully created!
+Thanks for using *MALVIN-XD* — your session has been successfully created!
 
 🔐 *Session ID:* Sent above  
 ⚠️ *Keep it safe!* Do NOT share this ID with anyone.
@@ -96,7 +96,7 @@ https://github.com/policeduafatima640/malvin-pair
 
 ——————
 
-> *© Powered by Dua Fatima*
+> *© Powered by Malvin King*
 Stay cool and hack smart. ✌🏻`; 
                         await sock.sendMessage(sock.user.id, {
 text: desc,
@@ -115,7 +115,7 @@ renderLargerThumbnail: true
                             let ddd = sock.sendMessage(sock.user.id, { text: e });
                             let desc = `Hey there, MALVIN-XD User!* 👋🏻
 
-Thanks for using *FATIMA-MD* — your session has been successfully created!
+Thanks for using *MALVIN-XD* — your session has been successfully created!
 
 🔐 *Session ID:* Sent above  
 ⚠️ *Keep it safe!* Do NOT share this ID with anyone.
@@ -132,7 +132,7 @@ https://github.com/policeduafatima/FATIMA-MD
 
 ——————
 
-> *© Powered by Dua Fatima*
+> *© Powered by Malvin King*
 Stay cool and hack smart. ✌🏻`;
                             await sock.sendMessage(sock.user.id, {
 text: desc,
@@ -168,7 +168,7 @@ showAdAttribution: true
             }
         }
     }
-   return await FATIMA_MD_PAIR_CODE();
+   return await MALVIN_XD_PAIR_CODE();
 });/*
 setInterval(() => {
     console.log("☘️ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...");
